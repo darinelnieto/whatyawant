116a31125dd3301285f85c00cdb19a233bd217ad
darinel nieto diaz <darinelnietodiaz2021@gmail.com>
2025-07-26 14:56:32 -0500

    Ajustes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path'); // You might need path for output directory later, good to include
 
 module.exports = {
+    // Set mode to 'development' for watch mode.
+    // Webpack automatically optimizes differently for 'production'.
+    mode: 'development',
+
     entry: './js/main.js',
     output: {
-        path: __dirname + '/js',
+        // __dirname points to the current directory of webpack.config.js
+        path: path.resolve(__dirname, 'js'), // Use path.resolve for robust pathing
         filename: 'main.bundle.js'
     },
 
@@ -11,15 +17,19 @@ module.exports = {
         rules: [
             {
                 test: /\.scss$/,
-                loader: [
-                    {loader: MiniCssExtractPlugin.loader},
-                    {loader: 'css-loader'},
-                    {loader: 'sass-loader'}
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' }
                 ]
             },
             {
                 test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-                loader: 'url-loader?limit=100000'
+                // Correct way to pass options to a loader
+                loader: 'url-loader',
+                options: {
+                    limit: 100000 // The 'limit' value moved to 'options'
+                }
             }
         ]
     },
@@ -29,4 +39,4 @@ module.exports = {
             filename: '../css/main.bundle.css'
         })
     ]
-}
+};
