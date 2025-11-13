fccebf27475331004e28f8779c977287a8dba858
darinel nieto diaz <darinelnietodiaz2021@gmail.com>
2025-11-13 11:29:51 -0500

    Url canonical

--- functions.php
+++ functions.php
@@ -336,3 +336,10 @@ function check_for_svg($data, $file, $filename, $mimes) {
     return $data;
 }
 add_filter('wp_check_filetype_and_ext', 'check_for_svg', 10, 4);
+
+add_filter('wpseo_canonical', function($canonical) {
+  if (is_singular('portfolio')) {
+    return get_permalink();
+  }
+  return $canonical;
+});
